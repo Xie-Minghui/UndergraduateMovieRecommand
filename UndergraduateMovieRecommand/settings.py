d81661c88b8d0ab74850e02935fa640eb007033c
mango <2075419247@qq.com>
2019-11-26 12:35:35 +0800

    添加了自己构造的评分数据集

--- UndergraduateMovieRecommand/settings.py
+++ UndergraduateMovieRecommand/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Behaviors',#添加app,谢明辉，9.2
+    'Comments',
+    'Filmmakers',
+    'History',
+    'Movies',
+    'Recommend',
+    'Unclassed',
+    'Users',
 ]
 
 MIDDLEWARE = [
