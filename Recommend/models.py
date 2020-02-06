d8504ab69528b85a728573d0f10216a8db4d1412
Lin <2657147493@qq.com>
2020-02-06 15:22:31 +0800

    完成home_movie方法，添加详情页接口

---
+++ Recommend/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from baseModel.BaseModel import BaseModel
+# Create your models here.
+class Recommend(BaseModel):
+    Recommend_id = models.AutoField(primary_key = True,verbose_name = "推荐列表的id")
+    user_name = models.ForeignKey('Users.User',on_delete = models.CASCADE)#外键约束
+    movie_name = models.ForeignKey('Movies.Movie',on_delete = models.CASCADE)
---
+++ Unclassed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UnclassedConfig(AppConfig):
+    name = 'Unclassed'
