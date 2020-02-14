6c13dc2f90f57aa6d4b93c8e9ca71da89b717898
Lin <2657147493@qq.com>
2020-02-14 16:14:40 +0800

    增加了明星、浏览历史、电影类别等模型及已有模型字段修改

--- Comments/models.py
+++ Comments/models.py
@@ -7,7 +7,7 @@ class Comments(BaseModel):
     comment_id = models.AutoField(primary_key=True)     #主键自增长
     user_name = models.ForeignKey('Users.User', on_delete=models.CASCADE)#外键约束
     movie_name = models.ForeignKey('Movies.Movie', on_delete=models.CASCADE)
-    content = models.CharField(max_length=300, verbose_name="评论内容") 
+    content = models.TextField(max_length=500, verbose_name="评论内容")
     agree_number = models.IntegerField(default=0, verbose_name="赞同量")
     disagree_number = models.IntegerField(default=0, verbose_name="踩")
 
--- History/models.py
+++ History/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from baseModel.BaseModel import BaseModel
+from Users.models import User
+from Movies.models import Movie
 # Create your models here.
+class BrowseHistory(BaseModel):#存放用户浏览历史
+    history_id = models.AutoField(primary_key=True, verbose_name='用户浏览记录id')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
